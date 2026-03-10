c4c4172a95577a0f8b1339582653f5012fdb5002
ingquimbay <quimbayc@javeriana.edu.co>
2026-03-10 12:13:49 -0500

    first commit. backend project creation

---
+++ backend/biblioteko/src/main/java/org/revature/data/BibliotekoApplication.java
@@ -0,0 +1,13 @@
+package org.revature.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotekoApplication.class, args);
+	}
+
+}
---
+++ backend/biblioteko/src/test/java/org/revature/data/BibliotekoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.revature.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

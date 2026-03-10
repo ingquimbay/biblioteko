b5dda69400ae1bf12be41e1c3f41f613d7ac86c1
ingquimbay <quimbayc@javeriana.edu.co>
2026-03-10 13:39:31 -0500

    Added Book entity

---
+++ backend/biblioteko/src/main/java/org/revature/data/entity/Book.java
@@ -0,0 +1,35 @@
+package org.revature.data.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "books")
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column
+	private String title;
+
+	@Column
+	private String author;
+
+	@Column
+	private String genre;
+
+	@Column
+	private int pages;
+
+	@Column
+	private int publishedYear;
+
+}
